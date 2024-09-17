869b130dabe4d84d96009abbd3339d05aafe2962
eawebmaster <eaweb20@gmail.com>
2024-09-17 14:04:46 +0000

    feat: setup dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,44 @@ import { ApiService } from './shared/services/api/api.service';
 import { addBoard, deleteBoard, fetchBoards, updateBoard,  } from './shared/state/board.actions';
 import { v4 as uuidv4 } from 'uuid'
 import { IBoard } from './shared/models/board';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {InputSwitchModule} from 'primeng/inputswitch';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    MatSidenavModule,
+    MatListModule,
+    CommonModule,
+    FormsModule,
+    MatIconModule,
+    InputSwitchModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'kanbanTaskManagement';
+  boards = ['Platform Launch', 'Marketing Plan', 'Roadmap']
   board:IBoard = {id:uuidv4(),name:'test board name',columns:[]}
+  selectedBoard = 'Platform Launch'
+  checked: boolean  = true
   constructor(
     private store:Store,
     private apiService:ApiService,  
   ) {
       this.store.dispatch(fetchBoards());
+  }
+
+  toggleTheme(){
+    this.checked = !this.checked;
+    console.log('theme toggled to :', this.checked);
   }
   addBoard(){
     this.store.dispatch(addBoard({board:this.board}))
@@ -32,5 +54,10 @@ export class AppComponent {
 
   deleteBoard(){
     this.store.dispatch(deleteBoard({id:''}))
+  }
+
+  selectBoard(boardName: string){
+    this.selectedBoard = boardName
+    console.log(boardName)
   }
 }
