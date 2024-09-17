2808e8033485df8c92d1fa5f80cafd79067faeca
eawebmaster <eaweb20@gmail.com>
2024-09-17 18:45:55 +0000

    feat: create board renderer component and add template for drag and drop

---
+++ src/app/components/board-renderer/board-renderer.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-board-renderer',
+  standalone: true,
+  imports: [CdkDropListGroup, CdkDropList, CdkDrag],
+  templateUrl: './board-renderer.component.html',
+  styleUrl: './board-renderer.component.sass'
+})
+export class BoardRendererComponent {
+  todos = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  all = [this.todos, this.done]
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+    console.log(this.all);
+  }
+}
