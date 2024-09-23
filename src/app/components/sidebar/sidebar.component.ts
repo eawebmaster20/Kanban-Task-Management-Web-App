a635238df6fdc1b0f46dbce60279a2a4737879e3
eawebmaster <eaweb20@gmail.com>
2024-09-23 07:46:05 +0000

    create component for sidebar display

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { ThemeTogglerComponent } from '../theme-toggler/theme-toggler.component';
+import { MatIconModule } from '@angular/material/icon';
+import { DataService } from '../../shared/services/data/data.service';
+import { MatListModule } from '@angular/material/list';
+import { Store } from '@ngrx/store';
+import { selectAllBoards, selectSelectedBoard } from '../../shared/state/board.selectors';
+import { AsyncPipe } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateBoardComponent } from '../modals/create-board/create-board.component';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    ThemeTogglerComponent,
+    MatIconModule,
+    MatListModule,
+    AsyncPipe,
+    CreateBoardComponent,
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.sass'
+})
+export class SidebarComponent {
+  allStoreBoards = selectAllBoards
+  selectedBoard = selectSelectedBoard
+  constructor(
+    public dataService:DataService,
+    public store:Store,
+    public dialog: MatDialog,
+  ){}
+
+
+  openDialog() {
+    console.log('dialog open')
+    const dialogRef = this.dialog.open(CreateBoardComponent);
+    dialogRef.afterClosed().subscribe({
+      next: (res) =>  {
+        if(!res.update){
+          this.dataService.createBoard(res.data)
+        }
+      },
+      error: (err) => console.error('Error:', err)
+    });
+  }
+}
